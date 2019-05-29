8e9a6218d400adc8418b497d9603b843a3aed978
Henry Miller <henry.miller18@outlook.com>
2019-05-29 11:44:42 +0200

    Added first implementation of water-moving script.
    Works by moving the two normal maps of the water surface shader.
    
    TODO:
    - make it possible to adapt movement speed of normal maps in Unity IDE (currently fixed value)
    - implement Phong-Shading for specular water surface

---
+++ TerrainBuilder/Assets/Scripts/WaterMovementScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WaterMovementScript : MonoBehaviour
+{
+    float xScroll1 = 0.1f;
+	float yScroll1 = 0.1f;
+	float xScroll2 = -0.1f;
+	float yScroll2 = -0.1f;
+    Renderer r;
+
+    void Start()
+    {
+        r = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        float xOffset1 = Time.time * xScroll1;
+		float yOffset1 = Time.time * yScroll1;
+		float xOffset2 = Time.time * xScroll2;
+		float yOffset2 = Time.time * yScroll2;
+        r.material.SetTextureOffset("_NormalMap1", new Vector2(xOffset1, yOffset1));
+		r.material.SetTextureOffset("_NormalMap2", new Vector2(xOffset2, yOffset2));
+    }
+}
